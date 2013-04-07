8cabfee01e766eb1aad722b244c8a9ad46d65e67
danielmariz <danielcsmariz@gmail.com>
2013-04-07 03:02:04 -0300

    estrutura de tema base para wp baseado no do matanza

---
+++ wp-clear-theme/theme/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+?>
+	</div><!-- Div Container -->
+	<footer class="container footer">
+		<div class="patrocinios">
+			<ul class="unstyled">
+				<li class="condor"><a href="">Condor</a></li>
+				<li class="condor"><a href="">King 7 Tattoo</a></li>
+				<li class="condor"><a href="">Acedo Audio</a></li>
+				<li class="condor"><a href="">Markadiabo</a></li>
+				<li class="condor"><a href="">Solid Sound</a></li>
+				<li class="condor"><a href="">Alba</a></li>
+				<li class="condor"><a href="">Tecnoforte Cables</a></li>
+				<li class="condor"><a href="">Seven</a></li>
+			</ul>
+		</div>
+	</footer>
+<script src="<?php echo get_template_directory_uri(); ?>/javascripts/libs/jquery/jquery-1.8.2.min.js" type="text/javascript"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
